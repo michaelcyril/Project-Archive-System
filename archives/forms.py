dce232def9cc053281a576c05d0bb73e1b0d52bf
ndaga1603 <davidndaga1603@gmail.com>
2024-05-01 22:41:53 +0300

    modifying forms

---
+++ archives/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from .models import Student, Staff
+
+
+class StudentForm(forms.ModelForm):
+    firstname = forms.CharField(max_length=100)
+    lastname = forms.CharField(max_length=100)
+    email = forms.EmailField()
+
+    class Meta:
+        model = Student
+        fields = [
+            "firstname",
+            "lastname",
+            "regNo",
+            "email",
+            "mobile",
+            "course",
+            "academic_year",
+            "department",
+            "gender",
+        ]
+
+class StaffForm(forms.ModelForm):
+    firstname = forms.CharField(max_length=100)
+    lastname = forms.CharField(max_length=100)
+    email = forms.EmailField()
+
+    class Meta:
+        model = Staff
+        fields = [
+            "firstname",
+            "lastname",
+            "email",
+            "staff_id",
+            "mobile",
+            "department",
+            "gender",
+        ]
